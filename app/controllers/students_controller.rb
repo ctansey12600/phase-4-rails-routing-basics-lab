71bafb2edd354b2ec69599b2df54c2de7a64dd15
Casey Tansey <ctansey12600@outlook.com>
2022-10-23 17:43:21 -0400

    Done.

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,10 @@
+class StudentsController < ApplicationController
+  def index
+    students = Student.all
+    render json: students
+  end
+  def grades
+    students_by_grades = Student.all.order(grade: :desc)
+    render json: students_by_grades
+  end
+end
