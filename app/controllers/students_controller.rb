63c100255f02a2c6d04360ac3bbb0aa3493bf0fd
Casey Tansey <ctansey12600@outlook.com>
2022-10-23 17:48:02 -0400

    Bonus Completed

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,4 +7,8 @@ class StudentsController < ApplicationController
     students_by_grades = Student.all.order(grade: :desc)
     render json: students_by_grades
   end
+  def highest_grade
+    student_highest_grade = Student.all.order(grade: :desc).first
+    render json: student_highest_grade
+  end
 end
